90f59cbf5d48293969f17801de228c4e2165a3c8
myfirstdebianpc <mahoro.ntwari@gmail.com>
2021-02-23 12:28:40 +0100

    libft complete

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -5,9 +5,11 @@ char	*ft_strncpy(char *dest, const char *src, size_t n)
 	char		*d;
 
 	d = dest;
-	while (n-- && ((*dest++ = *src++) != '\0'))
-		/* does nothing */;
+	while (n-- && ((*dest = *src) != '\0'))
+	{
+		dest++;
+		src++;
+	}
 	ft_bzero(dest, n);
 	return (d);
 }
-
